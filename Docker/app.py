a8063d82cd0789486b0118125df28e3cc7d78f53
Андрей Исаков <deflorator1980@yandex.ru>
2018-08-18 06:33:37 +0300

    my MVP

--- Docker/app.py
+++ Docker/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+# redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+redis = Redis('0.0.0.0', port=6379)
 
 @app.route('/')
 def hello():
